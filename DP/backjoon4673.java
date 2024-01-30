c9b6c1f5f30611b28599885419065a0a7abfa634
Jaehong Lee <55419868+compasstar@users.noreply.github.com>
2024-01-30 15:36:42 +0900

    백준 4673번 셀프 넘버

---
+++ DP/backjoon4673.java
@@ -0,0 +1,36 @@
+
+public class Main {
+
+
+	public static void main(String[] args) {
+		
+		boolean[] notSelfNumber = new boolean[10001];
+		
+		for (int i=1; i<=10000; i++) {
+			int d = d(i);
+			if (d <= 10000) {
+				notSelfNumber[d] = true;
+			}
+		}
+		
+		for (int i=1; i<=10000; i++) {
+			if (!notSelfNumber[i]) {
+				System.out.println(i);
+			}
+		}
+		
+
+
+	}
+	
+	private static int d(int num) {
+		int sum = num;
+		while (num > 0) {
+			sum += num % 10;
+			num /= 10;
+		}
+		return sum;
+	}
+
+
+}
